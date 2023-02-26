60c11f910bc510d07442e55c7282fface69cfa74
Maria P <maria@zoidpay.com>
2023-02-26 16:16:40 +0200

    Functional proxied request to blockchain-helper-api

--- build/index.js
+++ build/index.js
@@ -1,1 +1,22 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var express_http_proxy_1 = __importDefault(require("express-http-proxy"));
+var app = (0, express_1.default)();
+var userServiceProxy = (0, express_http_proxy_1.default)('https://6cvbb4t4465ecwdgtftvudcmce0xhuvc.lambda-url.eu-central-1.on.aws/api-docs');
+// Authentication
+app.use(function (req, res, next) {
+    // TODO: my authentication logic
+    next();
+});
+// Proxy request for elrond blockchain helpers
+// works only if it redirects all the routes here as *, not specific routes
+app.post('/*', function (req, res, next) {
+    userServiceProxy(req, res, next);
+});
+app.listen(3002, function () {
+    console.log("Example app listening on port ".concat(3002));
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello world!')
+import express from 'express';
+import httpProxy from 'express-http-proxy';
+
+const app = express();
+
+const userServiceProxy = httpProxy('https://6cvbb4t4465ecwdgtftvudcmce0xhuvc.lambda-url.eu-central-1.on.aws/api-docs')
+
+// Authentication
+app.use((req, res, next) => {
+  // TODO: my authentication logic
+  next()
+});
+
+// Proxy request for elrond blockchain helpers
+// works only if it redirects all the routes here as *, not specific routes
+app.post('/*', (req, res, next) => {
+  userServiceProxy(req, res, next);
+});
+
+app.listen(3002, () => {
+    console.log(`Example app listening on port ${3002}`);
+})
